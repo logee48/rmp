55160a5dbe80a36ab813715ad24ae36d7ac10c7a
logesh <logeshe9159@gmail.com>
2024-01-16 07:36:49 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,16 @@ app.use(
     origin: "https://rmp-test.onrender.com"
   })
 );
+app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();  
+}
+app.use(allowCrossDomain);
+
 app.use(cookieParser());
 
 app.use(express.json());
