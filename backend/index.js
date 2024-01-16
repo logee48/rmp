52dfcbb46e029ef03e5ef01a0cdc7707b1fb4c51
logesh <logeshe9159@gmail.com>
2024-01-16 08:22:25 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -25,15 +25,6 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
-app.use(cors());
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-}
-app.use(allowCrossDomain);
 app.use(cookieParser());
 
 app.use(express.json());
