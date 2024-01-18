477544f9142d8cdc977b0ccfef105262a8355a7a
logesh <logeshe9159@gmail.com>
2024-01-18 19:43:56 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -12,18 +12,23 @@ mongoose
   .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
 
+  app.use(cors())
+  app.use(
+    cors({
+      origin: ["https://rmp004.netlify.app/"],
+      // origin: ["http://localhost:3000"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+  
+  app.use(express.json());
+  app.use("/", authRoute);
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-app.use(
-  cors({
-    origin: ["https://rmp004.netlify.app/"],
-    // origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 
-app.use(express.json());
-app.use("/", authRoute);
